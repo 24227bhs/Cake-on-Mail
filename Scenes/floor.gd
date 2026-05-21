f1b1fde2d1cebba6a89f730dcc7de8e090079091
24227bhs <24227@burnside.school.nz>
2026-05-22 11:46:01 +1200

    added death hitboxes
    added death area, so you can reset the level. tweaked a couple variables.

---
+++ Scenes/floor.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("whoops!")
+	get_tree().reload_current_scene()
