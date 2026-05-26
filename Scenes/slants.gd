81cf59cf6449bf231743e13fffb65bf43c8bc236
24227bhs <24227@burnside.school.nz>
2026-05-26 14:42:24 +1200

    small variable tweak
    changed a few variables and added objects

--- Scenes/slants.gd
+++ Scenes/slants.gd
@@ -3,6 +3,6 @@ extends StaticBody2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("slant left"):
-		rotate(45)
+		rotate(15)
 	if Input.is_action_just_pressed("slant right"):
-		rotate(-45)
+		rotate(-15)
