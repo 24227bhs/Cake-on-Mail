fdc6fbd6826bf0b24a34a32ad2bfdff2863ddb1a
24227bhs <24227@burnside.school.nz>
2026-05-27 12:46:28 +1200

    ADDED SOFTBODY2D ADDON
    tried doing some plate stuff, gonna work with softbody addon hopefully it'll work :)

--- Scenes/slants.gd
+++ Scenes/slants.gd
@@ -1,8 +1,8 @@
-extends StaticBody2D
+extends CharacterBody2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("slant left"):
-		rotate(15)
-	if Input.is_action_just_pressed("slant right"):
-		rotate(-15)
+	if Input.is_action_pressed("slant left"):
+		get_parent().rotate(1)
+	if Input.is_action_pressed("slant right"):
+		get_parent().rotate(-1)
