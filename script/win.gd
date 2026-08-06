4828224c2ff3e827680c2c200378778064375aa4
24227bhs <24227@burnside.school.nz>
2026-08-07 09:25:46 +1200

    levellevel and bugfix
    fixed bug
    did more level making

--- script/win.gd
+++ script/win.gd
@@ -5,4 +5,4 @@ extends Area2D
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("cake"):
 		print("you win!")
-		get_tree().change_scene_to_file('res://Scenes/levelselect.tscn)
+		get_tree().change_scene_to_file("res://Scenes/levelselect.tscn")
