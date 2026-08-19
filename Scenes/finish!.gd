92de2f89340bef573a0830064bd02694a340342c
24227bhs <24227@burnside.school.nz>
2026-08-20 11:45:52 +1200

    made the finish screen work more
    made the stopwatch label work, and made it show when you finish the level (: so much waow meow

--- Scenes/finish!.gd
+++ Scenes/finish!.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 @onready var time = $MarginContainer/VBoxContainer/Label3
 @onready var wawa = $Label4
 @onready var leveltext = $MarginContainer/VBoxContainer/Label2
-#@onready var stopwatch = $"../speedrun timer"
+@onready var stopwatch = $"../speedrun timer"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,6 +12,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	testlabel.text = "the code can work"
-#	time.text = stopwatch
 	wawa.text = str(testlabel.text)
 	leveltext.text = name
--- script/win.gd
+++ script/win.gd
@@ -1,8 +1,18 @@
 extends Area2D
 
-
+@onready var stopwatch = $"../player/speedrun timer"
+@onready var finish = $"../player/finish!"
+@onready var win = $"."
+var won = false
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("cake"):
 		print("you win!")
-		get_tree().change_scene_to_file("res://Scenes/levelselect.tscn")
+		#get_tree().change_scene_to_file("res://Scenes/levelselect.tscn")
+		won = true
+		_wintrue()
+
+func _wintrue():
+	finish.show()
+	stopwatch.transform.origin = Vector2(900, 130)
+	get_tree().paused = true
