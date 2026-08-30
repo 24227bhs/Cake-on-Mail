296ab13c2515e2e2d91ba27898363592ea81ebb0
24227bhs <24227@burnside.school.nz>
2026-08-30 20:57:02 +1200

    some changes and stuffs
    added level 3 to level select, need to change canvas layer to its own scene so the code is easier.

--- Scenes/finish!.gd
+++ Scenes/finish!.gd
@@ -4,6 +4,7 @@ extends CanvasLayer
 @onready var wawa = $Label4
 @onready var leveltext = $MarginContainer/VBoxContainer/Label2
 @onready var stopwatch = $"../speedrun timer"
+@onready var GUI = $"."
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -18,9 +19,10 @@ func _process(delta: float) -> void:
 	
 
 
-func _on_button_pressed() -> void:
+
+
+func _on_level_select_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/levelselect.tscn")
 
-
-func _on_button_2_pressed() -> void:
+func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/mainmenu.tscn")
--- script/levelselect.gd
+++ script/levelselect.gd
@@ -12,3 +12,7 @@ func _on_levelone_pressed() -> void:
 
 func _on_leveltwo_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/leveltwo.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/levelthree.tscn")
